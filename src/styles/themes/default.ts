6290752f5ca80673a304d4bc91245c010a04adcc
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-02-25 15:39:31 -0300

    chore: Add styles/themes/default

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,25 @@
+// Light theme - primary/default
+export const defaultTheme = {
+  white: '#FFF',
+
+  'neutral-100': '#E9EDF5',
+  'neutral-200': '#E1E1E6',
+  'neutral-300': '#EDEDED',
+  'neutral-400': '#E6E5E5',
+  'neutral-500': '#A8A8B3',
+  'neutral-600': '#8d8686',
+  'neutral-700': '#29292E',
+  'neutral-800': '#202024',
+  'neutral-900': '#121214',
+
+  'primary-300': '#FFB2C2',
+  'primary-500': '#E51C44',
+  'primary-700': '#76081E',
+
+  'secondary-300': '#ABB1CC',
+  'secondary-500': '#495BCC',
+  'secondary-700': '#0A1033',
+
+  'semantic-positive': '#04D361',
+  'semantic-negative': '#FF5343',
+}
