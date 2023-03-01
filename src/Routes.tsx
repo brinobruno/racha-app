62d4c315ff26f24350de0a37d2e1ba1e4ce965af
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-01 18:48:28 -0300

    chore: Add route for signInPage

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { DefaultLayout } from './layouts/DefaultLayout'
+import Signin from './pages/Signin'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
       </Route>
     </Routes>
   )
