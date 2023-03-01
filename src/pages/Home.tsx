f203700ed29337f25dc4114eb8e0aa728134ed48
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-01 17:12:17 -0300

    feat: Add toastify example

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,28 @@
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 export function Home() {
+  const notify = () => toast('Wow so easy!')
+
   return (
     <section>
       <h1>Home</h1>
 
       <input type="text" placeholder="input" />
-      <button>Button</button>
+      <button onClick={notify}>Notify</button>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </section>
   )
 }
