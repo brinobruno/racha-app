23f9e434705fb174d7d8ef84000d50f42df0ff2d
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-03 20:59:22 -0300

    chore: Update signIn draft with contants

--- src/services/signInUser.ts
+++ src/services/signInUser.ts
@@ -1,18 +1,15 @@
+import { BASE_URL, headers } from '../Constants'
+
 interface ISignInRequest {
   email: string | undefined
   password: string | undefined
 }
 
 export async function signInUser({ email, password }: ISignInRequest) {
-  const headers = {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
-  }
-
   const bodyRequest = JSON.stringify({ email, password })
 
   try {
-    return await fetch('http://localhost:3333/users/login', {
+    return await fetch(`${BASE_URL}/users/login`, {
       method: 'POST',
       headers,
       body: bodyRequest,
