73718c7a18f24376aacb8e6d2fd1ee42f8f738e6
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-04 11:04:18 -0300

    feat: Create dashboard page

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,28 @@
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+export function Dashboard() {
+  const notify = () => toast('Wow so easy!')
+
+  return (
+    <section>
+      <h1>Dashboard</h1>
+
+      <input type="text" placeholder="input" />
+      <button onClick={notify}>Notify</button>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </section>
+  )
+}
