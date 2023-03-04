d22d23fbeb2ccc07b9d9f3621d51c09944ee6d7d
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-04 14:29:30 -0300

    feat: Export styles for layout container component

---
+++ src/layouts/auth/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 68rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 1180px) {
+    padding: 0 3.125rem;
+  }
+
+  @media (max-width: 350px) {
+    padding: 0 1.5rem;
+  }
+
+  img {
+    user-select: none;
+  }
+`
