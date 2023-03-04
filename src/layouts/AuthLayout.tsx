18d5a67ff200a4f224f8d16bfbea1ca5ae2d47f2
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-04 13:50:35 -0300

    feat: Add header component to authlayout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import { Header } from './auth/header'
 
 export function AuthLayout() {
   return (
     <>
-      <header>Auth</header>
+      <Header />
 
       {/* Space for content, specific to every page */}
       <Outlet />
 
-      <footer>Footer</footer>
+      {/* <footer>Footer</footer> */}
     </>
   )
 }
