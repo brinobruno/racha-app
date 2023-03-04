f78b49787356d64b5c99fe37b04891aa3b65d0d3
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-04 14:09:22 -0300

    chore: Add desktop-cover to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,13 @@
-import { ToastContainer, toast } from 'react-toastify'
+import Cover from './../assets/desktop-cover.webp'
+
 import 'react-toastify/dist/ReactToastify.css'
 
 export function Home() {
-  const notify = () => toast('Wow so easy!')
-
   return (
     <section>
       <h1>Home</h1>
 
-      <input type="text" placeholder="input" />
-      <button onClick={notify}>Notify</button>
-
-      <ToastContainer
-        position="bottom-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
+      <img src={Cover} alt="" />
     </section>
   )
 }
