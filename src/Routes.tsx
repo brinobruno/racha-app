c02fe873daf8fabfa612b9c379a0370f5e8d5d62
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-04 11:14:51 -0300

    Add two routes - one for authlayout and one for defaultlayout

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { DefaultLayout } from './layouts/DefaultLayout'
-import Signin from './pages/Signin'
+import { AuthLayout } from './layouts/AuthLayout'
+import { Signin } from './pages/Signin'
+import { Dashboard } from './pages/Dashboard'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
+      <Route path="/" element={<AuthLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
+      </Route>
+
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
       </Route>
     </Routes>
   )
