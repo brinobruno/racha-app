f71f8407b447391495eb96ba9bf6e9ac3e18f0a7
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-04 14:49:48 -0300

    chore: Add react-router navlink

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,13 +2,22 @@ import Cover from './../../assets/desktop-cover.webp'
 
 import 'react-toastify/dist/ReactToastify.css'
 import { Container } from './styles'
+import { NavLink } from 'react-router-dom'
 
 export function Home() {
   return (
     <Container>
-      <img src={Cover} alt="" />
+      <div>
+        <img src={Cover} alt="" />
+      </div>
 
-      <h1>Gerencie times para jogar com os amigos</h1>
+      <div>
+        <h1>Gerencie times para jogar com os amigos</h1>
+
+        <NavLink to="/signin" title="Entrar">
+          Entrar
+        </NavLink>
+      </div>
     </Container>
   )
 }
