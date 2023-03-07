35a6e0a301d5ef28c947a98d2b63e60f3b035fc3
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-07 18:08:59 -0300

    chore: Add query client context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import { Router } from './Routes'
 import { GlobalStyle } from './styles/global'
 import { ThemeContextProvider } from './contexts/ThemeContext'
 
+const queryClient = new QueryClient()
+
 export function App() {
   return (
     <>
-      <BrowserRouter>
-        <ThemeContextProvider>
-          <Router />
-          <GlobalStyle />
-        </ThemeContextProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ThemeContextProvider>
+            <Router />
+            <GlobalStyle />
+          </ThemeContextProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
     </>
   )
 }
