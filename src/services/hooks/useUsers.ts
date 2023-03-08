7e9b0b9741bb7c9aa0d6ecd807488a8abeb1c016
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-08 11:57:34 -0300

    refactor: Split useUsers hook into a separate function

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -10,33 +10,31 @@ interface UsersDataResponse {
   created_at: Date
 }
 
+async function getUsers() {
+  const response = await api.get(`/users/`, {
+    headers,
+  })
+
+  const data = response.data
+
+  const users = data.users.map((user: UsersDataResponse) => {
+    return {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      createdAt: new Date(user.created_at).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    }
+  })
+
+  return users
+}
+
 export function useUsers() {
-  return useQuery(
-    ['users'],
-    async () => {
-      const response = await api.get(`/users/`, {
-        headers,
-      })
-
-      const data = response.data
-
-      const users = data.users.map((user: UsersDataResponse) => {
-        return {
-          id: user.id,
-          username: user.username,
-          email: user.email,
-          createdAt: new Date(user.created_at).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric',
-          }),
-        }
-      })
-
-      return users
-    },
-    {
-      staleTime: 1000 * 5, // WIll be fresh for 5 seconds (!obsolete)
-    },
-  )
+  return useQuery('users', getUsers, {
+    staleTime: 1000 * 5, // WIll be fresh for 5 seconds (!obsolete)
+  })
 }
