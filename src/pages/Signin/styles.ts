a68cd8c37176c18fdcccbf8bdbb320c6d9f33b02
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-08 12:24:48 -0300

    chore: Remove form components from Signin page

--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -21,27 +21,3 @@ export const Container = styled.div`
     font-weight: 600;
   }
 `
-
-export const SignInForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 1.25rem;
-
-  max-width: 21.625rem;
-  width: 100%;
-
-  button {
-    margin: 1rem 0 4rem;
-    width: 100%;
-  }
-`
-
-export const FieldContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-
-  width: 100%;
-`
