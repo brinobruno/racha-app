7f6a1d04840fd61d8243bdcc3bb77f421e22db8d
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-09 18:19:53 -0300

    feat: Create UseUsersContext with addUser and getUser methods

---
+++ src/hooks/UseUsersContext.tsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react'
+import { IUser, UserContext } from '../contexts/UserContext'
+
+export const UseUsersContext = () => {
+  const { user, setUser } = useContext(UserContext)
+
+  function addUser({ id, username, email }: IUser) {
+    setUser({ id, username, email })
+
+    return user
+  }
+
+  function getUser() {
+    console.log(user)
+  }
+
+  return {
+    addUser,
+    getUser,
+  }
+}
