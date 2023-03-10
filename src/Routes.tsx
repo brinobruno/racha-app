b35f3b513ceda62620d8e02093f057eda02250bb
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-10 15:19:07 -0300

    feat: Divide ProtectedRoute into separate file

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,37 +1,14 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+
 import { Home } from './pages/Home'
 import { DefaultLayout } from './layouts/default/DefaultLayout'
 import { AuthLayout } from './layouts/auth/AuthLayout'
 import { Signin } from './pages/Signin'
 import { SignUp } from './pages/Signup'
 import { Dashboard } from './pages/Dashboard'
-import { USER_SESSION_STORAGE_KEY } from './Constants'
-
-export type ProtectedRouteProps = {
-  isAuthenticated: boolean
-  authenticationPath: string
-  // eslint-disable-next-line no-undef
-  outlet: JSX.Element
-}
-
-export default function ProtectedRoute({
-  isAuthenticated,
-  authenticationPath,
-  outlet,
-}: ProtectedRouteProps) {
-  if (isAuthenticated) {
-    return outlet
-  } else {
-    return <Navigate to={{ pathname: authenticationPath }} />
-  }
-}
-
-const sessionId = localStorage.getItem(USER_SESSION_STORAGE_KEY)
-
-const defaultProtectedRouteProps: Omit<ProtectedRouteProps, 'outlet'> = {
-  isAuthenticated: !!sessionId,
-  authenticationPath: '/signin',
-}
+import ProtectedRoute, {
+  defaultProtectedRouteProps,
+} from './services/auth/ProtectedRoute'
 
 export function Router() {
   return (
---
+++ src/services/auth/ProtectedRoute.tsx
@@ -0,0 +1,28 @@
+import { Navigate } from 'react-router-dom'
+import { USER_SESSION_STORAGE_KEY } from '../../Constants'
+
+export type ProtectedRouteProps = {
+  isAuthenticated: boolean
+  authenticationPath: string
+  // eslint-disable-next-line no-undef
+  outlet: JSX.Element
+}
+
+export default function ProtectedRoute({
+  isAuthenticated,
+  authenticationPath,
+  outlet,
+}: ProtectedRouteProps) {
+  if (isAuthenticated) {
+    return outlet
+  } else {
+    return <Navigate to={{ pathname: authenticationPath }} />
+  }
+}
+
+const sessionId = localStorage.getItem(USER_SESSION_STORAGE_KEY)
+
+export const defaultProtectedRouteProps: Omit<ProtectedRouteProps, 'outlet'> = {
+  isAuthenticated: !!sessionId,
+  authenticationPath: '/signin',
+}
