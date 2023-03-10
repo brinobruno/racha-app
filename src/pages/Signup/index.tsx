dacaf3b9b78e0d44d4e4192b4f9be5b899f1c3dc
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-10 15:22:37 -0300

    feat: Apply same signin page logic for logged-in on SignUp page

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,9 +1,21 @@
-import { NavLink } from 'react-router-dom'
+import { useEffect } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 import { Container } from './styles'
 import { SignUpForm } from '../../components/SignUpForm'
+import { USER_SESSION_STORAGE_KEY } from '../../Constants'
 
 export function SignUp() {
+  const navigate = useNavigate()
+
+  const sessionId = localStorage.getItem(USER_SESSION_STORAGE_KEY)
+
+  useEffect(() => {
+    if (sessionId) {
+      navigate('/dashboard')
+    }
+  })
+
   return (
     <Container>
       <h1>Olá! Registre-se para começar 👋</h1>
