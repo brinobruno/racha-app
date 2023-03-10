a0e9bb6f4ba89f201a9e3d6c501af35036e5ae06
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-10 15:10:26 -0300

    feat: Create ProtectedRoutes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,37 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { DefaultLayout } from './layouts/default/DefaultLayout'
 import { AuthLayout } from './layouts/auth/AuthLayout'
 import { Signin } from './pages/Signin'
 import { SignUp } from './pages/Signup'
 import { Dashboard } from './pages/Dashboard'
+import { USER_SESSION_STORAGE_KEY } from './Constants'
+
+export type ProtectedRouteProps = {
+  isAuthenticated: boolean
+  authenticationPath: string
+  // eslint-disable-next-line no-undef
+  outlet: JSX.Element
+}
+
+export default function ProtectedRoute({
+  isAuthenticated,
+  authenticationPath,
+  outlet,
+}: ProtectedRouteProps) {
+  if (isAuthenticated) {
+    return outlet
+  } else {
+    return <Navigate to={{ pathname: authenticationPath }} />
+  }
+}
+
+const sessionId = localStorage.getItem(USER_SESSION_STORAGE_KEY)
+
+const defaultProtectedRouteProps: Omit<ProtectedRouteProps, 'outlet'> = {
+  isAuthenticated: !!sessionId,
+  authenticationPath: '/signin',
+}
 
 export function Router() {
   return (
@@ -16,7 +43,15 @@ export function Router() {
       </Route>
 
       <Route path="/" element={<DefaultLayout />}>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute
+              {...defaultProtectedRouteProps}
+              outlet={<Dashboard />}
+            />
+          }
+        />
       </Route>
     </Routes>
   )
