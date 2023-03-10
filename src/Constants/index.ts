3fc364d7ff3cd9acb02916839c31185eb0538ad8
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-10 15:11:06 -0300

    chore: Export user session and theme preference storage key

--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -5,4 +5,13 @@ const headers = {
   'Access-Control-Allow-Origin': BASE_URL,
 }
 
-export { BASE_URL, headers }
+const USER_SESSION_STORAGE_KEY = '@racha-app:session-id-1.0.0'
+
+const THEME_PREFERENCE_STORAGE_KEY = '@racha-app:theme-1.0.0'
+
+export {
+  BASE_URL,
+  headers,
+  USER_SESSION_STORAGE_KEY,
+  THEME_PREFERENCE_STORAGE_KEY,
+}
