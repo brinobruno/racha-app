4bf18bbacc06b93e1d2cc352541424a698681509
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-11 17:59:40 -0300

    chore: Use js-cookie method on useSession

--- src/hooks/useSession.tsx
+++ src/hooks/useSession.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { getCookie } from 'typescript-cookie'
+import Cookie from 'js-cookie'
 
 import { USER_SESSION_STORAGE_KEY } from '../constants'
 
@@ -8,7 +8,7 @@ export function useSession() {
   const navigate = useNavigate()
 
   useEffect(() => {
-    const sessionId = getCookie(USER_SESSION_STORAGE_KEY)
+    const sessionId = Cookie.get(USER_SESSION_STORAGE_KEY)
 
     if (sessionId) {
       navigate('/dashboard')
