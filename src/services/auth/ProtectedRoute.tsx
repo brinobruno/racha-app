23b8c488fcd214724c1c171363947b2dcce7a005
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-13 19:15:39 -0300

    fix: Add real-time (render) verification of isAuthenticated data

--- src/services/auth/ProtectedRoute.tsx
+++ src/services/auth/ProtectedRoute.tsx
@@ -4,27 +4,20 @@ import Cookie from 'js-cookie'
 import { USER_SESSION_STORAGE_KEY } from '../../constants'
 
 export type ProtectedRouteProps = {
-  isAuthenticated: boolean
   authenticationPath: string
   // eslint-disable-next-line no-undef
   outlet: JSX.Element
 }
 
 export default function ProtectedRoute({
-  isAuthenticated,
   authenticationPath,
   outlet,
 }: ProtectedRouteProps) {
+  const isAuthenticated = !!Cookie.get(USER_SESSION_STORAGE_KEY)
+
   if (isAuthenticated) {
     return outlet
   } else {
     return <Navigate to={{ pathname: authenticationPath }} />
   }
-}
-
-const sessionId = Cookie.get(USER_SESSION_STORAGE_KEY)
-
-export const defaultProtectedRouteProps: Omit<ProtectedRouteProps, 'outlet'> = {
-  isAuthenticated: !!sessionId,
-  authenticationPath: '/signin',
 }
