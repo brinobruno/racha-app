e4f9cb439f9d02cee80292896afdc096a8fcd29e
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-14 18:01:29 -0300

    test: UserContextProvider - user context value is null by default

--- src/__tests__/UserContext.test.tsx
+++ src/__tests__/UserContext.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import { UserContextProvider } from './../contexts/UserContext'
+import { UserContext, UserContextProvider } from './../contexts/UserContext'
 import { describe, test } from 'vitest'
 
 describe('UserContextProvider', () => {
@@ -12,5 +12,20 @@ describe('UserContextProvider', () => {
 
     const childComponent = screen.getByTestId('child-component')
     t.expect(document.contains(childComponent)).toBe(true)
+  })
+
+  test('user context value is null by default', (t) => {
+    render(
+      <UserContextProvider>
+        <UserContext.Consumer>
+          {({ user }) => (
+            <div data-testid="user-value">{JSON.stringify(user)}</div>
+          )}
+        </UserContext.Consumer>
+      </UserContextProvider>,
+    )
+
+    const userValue = screen.getByTestId('user-value')
+    t.expect(userValue.textContent, 'null')
   })
 })
