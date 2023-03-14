1201d03d47ac4a8125d607d7b7114ee42dfd34fb
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-14 18:01:48 -0300

    test: UserContextProvider - setUser updates user context value

--- src/__tests__/UserContext.test.tsx
+++ src/__tests__/UserContext.test.tsx
@@ -28,4 +28,41 @@ describe('UserContextProvider', () => {
     const userValue = screen.getByTestId('user-value')
     t.expect(userValue.textContent, 'null')
   })
+
+  test('setUser updates user context value', (t) => {
+    render(
+      <UserContextProvider>
+        <UserContext.Consumer>
+          {({ setUser }) => (
+            <button
+              onClick={() =>
+                setUser({
+                  id: '1',
+                  username: 'testuser',
+                  email: 'test@test.com',
+                })
+              }
+            >
+              Set User
+            </button>
+          )}
+        </UserContext.Consumer>
+        <UserContext.Consumer>
+          {({ user }) => (
+            <div data-testid="user-value">{JSON.stringify(user)}</div>
+          )}
+        </UserContext.Consumer>
+      </UserContextProvider>,
+    )
+
+    const setUserButton = screen.getByText('Set User')
+    setUserButton.click()
+
+    const userValues = screen.getAllByTestId('user-value')
+    t.expect(userValues.length).toBe(1)
+    t.expect(
+      userValues[0].textContent,
+      JSON.stringify({ id: '1', username: 'testuser', email: 'test@test.com' }),
+    )
+  })
 })
