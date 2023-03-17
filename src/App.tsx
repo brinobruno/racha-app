8336d703260552e37bfd3adaacd9573d9d932636
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-16 22:41:20 -0300

    chore: Replace full cookie consent declaration to CookieNotice import

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
-import CookieConsent from 'react-cookie-consent'
 
 import { Router } from './Routes'
 import { GlobalStyle } from './styles/global'
 import { UserContextProvider } from './contexts/UserContext'
 import { ThemeContextProvider } from './contexts/ThemeContext'
-import { COOKIE_CONSENT_STORAGE_KEY } from './constants'
+import { CookieNotice } from './components/CookieNotice'
 
 const queryClient = new QueryClient()
 
@@ -20,27 +19,7 @@ export function App() {
             <ThemeContextProvider>
               <Router />
 
-              <CookieConsent
-                debug={true}
-                location="bottom"
-                cookieName={COOKIE_CONSENT_STORAGE_KEY}
-                buttonText="Tudo bem 🍪"
-                style={{
-                  background: '#495BCC',
-                  color: '#FFF',
-                  fontSize: '1rem',
-                }}
-                buttonStyle={{
-                  background: '#0A1033',
-                  color: '#FFF',
-                  fontSize: '1rem',
-                  borderRadius: '5px',
-                }}
-                sameSite="strict"
-                expires={180} // Number of days before the cookie expires
-              >
-                Esse site usa cookies para melhorar sua experiência.
-              </CookieConsent>
+              <CookieNotice />
 
               <GlobalStyle />
             </ThemeContextProvider>
