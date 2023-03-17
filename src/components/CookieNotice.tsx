f89b7ff03cd6be774f277f63a1e024d5763ba742
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-16 22:41:00 -0300

    feat: Create cookie notice component to import into app

---
+++ src/components/CookieNotice.tsx
@@ -0,0 +1,28 @@
+import CookieConsent from 'react-cookie-consent'
+import { COOKIE_CONSENT_STORAGE_KEY } from '../constants'
+
+export function CookieNotice() {
+  return (
+    <CookieConsent
+      debug={true}
+      location="bottom"
+      cookieName={COOKIE_CONSENT_STORAGE_KEY}
+      buttonText="Tudo bem 🍪"
+      style={{
+        background: '#495BCC',
+        color: '#FFF',
+        fontSize: '1rem',
+      }}
+      buttonStyle={{
+        background: '#0A1033',
+        color: '#FFF',
+        fontSize: '1rem',
+        borderRadius: '5px',
+      }}
+      sameSite="strict"
+      expires={180} // Number of days before the cookie expires
+    >
+      Esse site usa cookies para melhorar sua experiência.
+    </CookieConsent>
+  )
+}
