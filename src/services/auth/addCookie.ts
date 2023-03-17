5fc836aec63ab9d65ec00071cdf859730ccd2e27
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-16 22:53:11 -0300

    docs: Add comment on setCookie expires attribute

--- src/services/auth/addCookie.ts
+++ src/services/auth/addCookie.ts
@@ -2,7 +2,7 @@ import { setCookie } from 'typescript-cookie'
 
 export async function addCookie(key: string, value: string) {
   return setCookie(key, value, {
-    expires: 90,
+    expires: 90, // Expires after 90 days
     path: '',
     sameSite: 'Strict',
     // secure: true,
