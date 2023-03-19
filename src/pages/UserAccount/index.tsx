8b6362ce69d94495c00a9a12027c3d73bdd6aeef
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-19 20:10:22 -0300

    feat: Add user data from context

--- src/pages/UserAccount/index.tsx
+++ src/pages/UserAccount/index.tsx
@@ -7,11 +7,17 @@ export function UserAccount() {
 
   const user = getUser()
 
-  console.log(user?.email)
+  console.log(user)
 
   return (
     <section>
       <h1>Minha conta</h1>
+
+      <ul>
+        <li>{user?.id}</li>
+        <li>{user?.username}</li>
+        <li>{user?.email}</li>
+      </ul>
     </section>
   )
 }
