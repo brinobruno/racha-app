6813f923011983f95b84a55506723422503ce0e9
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-20 18:21:44 -0300

    feat: Add validation on constants headers

--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -1,15 +1,23 @@
-const BASE_URL = import.meta.env.VITE_RACHA_API_ENDPOINT
+import { Cookies } from 'typescript-cookie'
 
-const headers = {
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': BASE_URL,
-}
+const BASE_URL = import.meta.env.VITE_RACHA_API_ENDPOINT
 
 const USER_SESSION_STORAGE_KEY = '@racha-app:session-id-1.0.0'
 
 const THEME_PREFERENCE_STORAGE_KEY = '@racha-app:theme-1.0.0'
 
 const COOKIE_CONSENT_STORAGE_KEY = '@racha-app:cookie-consent-1.0.0'
+
+const sessionIdValue = Cookies.get(USER_SESSION_STORAGE_KEY)
+
+const headers = {
+  'Access-Control-Allow-Credentials': true,
+  withCredentials: true,
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': BASE_URL,
+  // eslint-disable-next-line prettier/prettier
+  'Cookies': `${sessionIdValue}`,
+}
 
 export {
   BASE_URL,
