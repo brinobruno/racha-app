f7f9f1a694d5cefc63d5e57babb7d5ed2f30c57f
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-20 18:25:52 -0300

    chore: Display user data on user account

--- src/pages/UserAccount/index.tsx
+++ src/pages/UserAccount/index.tsx
@@ -11,6 +11,10 @@ export function UserAccount() {
     <section>
       <h1>Minha conta</h1>
 
+      <span>{user?.email}</span>
+      <br />
+      <span>{user?.username}</span>
+
       {isLoading ? <span>Loading</span> : ''}
       {error ? <span>Error</span> : ''}
     </section>
