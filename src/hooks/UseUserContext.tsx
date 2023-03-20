e4696b31c822112515d26061b8ab09c6643f5ddc
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-20 18:21:30 -0300

    feat: Save userId to cookies

--- src/hooks/UseUserContext.tsx
+++ src/hooks/UseUserContext.tsx
@@ -1,11 +1,14 @@
 import { useContext } from 'react'
 import { IUser, UserContext } from '../contexts/UserContext'
+import { addCookie } from '../services/auth/addCookie'
 
 export const UseUserContext = () => {
   const { user, setUser } = useContext(UserContext)
 
   function addUser({ id, username, email }: IUser) {
     setUser({ id, username, email })
+
+    addCookie('userid', id || '')
 
     return user
   }
