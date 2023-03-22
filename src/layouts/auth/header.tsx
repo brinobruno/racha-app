5534dfd4d6c21594822ec563b5bdaf15e9f0004e
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-21 22:04:23 -0300

    style: Use src to replace relative path on /layouts

--- src/layouts/auth/header.tsx
+++ src/layouts/auth/header.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom'
-import logoWhite from './../../assets/logo-white.svg'
+import logoWhite from 'src/assets/logo-white.svg'
 
 export function Header() {
   return (
