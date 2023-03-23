1f8fdf773e7e51a6475067113866fb7fab2bf9a0
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-22 23:25:27 -0300

    feat: Create styles for header layout (logged-in)

--- src/layouts/default/styles.ts
+++ src/layouts/default/styles.ts
@@ -14,3 +14,49 @@ export const LayoutContainer = styled.div`
     padding: 0 1.5rem;
   }
 `
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 6rem;
+
+  > div {
+    width: 100%;
+    max-width: 200px;
+  }
+`
+
+export const NavBar = styled.nav`
+  ul {
+    display: flex;
+    justify-content: center;
+    gap: 32px;
+  }
+`
+
+export const Logo = styled.div`
+  display: flex;
+  justify-content: flex-start;
+
+  width: 100%;
+  height: 100%;
+  max-height: 56px;
+
+  img {
+    height: 100%;
+    max-height: 56px;
+  }
+`
+
+export const ProfileItem = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+
+  img {
+    height: 100%;
+    max-height: 56px;
+  }
+`
