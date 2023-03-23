38b65b4d48f2e93b1b0a73ff6ed27d2af71241f4
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-23 15:36:36 -0300

    chore: Adapt layout according to styles defined

--- src/layouts/default/header.tsx
+++ src/layouts/default/header.tsx
@@ -4,6 +4,7 @@ import LogoWhite from 'src/assets/logo-white.svg'
 import ProfilePic from 'src/assets/profile-pic.png'
 import {
   HeaderContainer,
+  HeaderItems,
   Logo,
   NavBar,
   ProfileItem,
@@ -14,33 +15,35 @@ export function Header() {
   return (
     <>
       <HeaderContainer>
-        <Logo>
-          <NavLink to="/">
-            <img src={LogoWhite} alt="Início" />
-          </NavLink>
-        </Logo>
+        <HeaderItems>
+          <Logo>
+            <NavLink to="/">
+              <img src={LogoWhite} alt="Início" />
+            </NavLink>
+          </Logo>
 
-        <NavBar>
-          <ul>
-            <li>
-              <NavLink to="/dashboard">Início</NavLink>
-            </li>
-            <li>
-              <NavLink to="/dashboard">Meus times</NavLink>
-            </li>
-            <li>
-              <NavLink to="/account">Minha conta</NavLink>
-            </li>
-          </ul>
-        </NavBar>
+          <NavBar>
+            <ul>
+              <li>
+                <NavLink to="/dashboard">Início</NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard">Meus times</NavLink>
+              </li>
+              <li>
+                <NavLink to="/account">Minha conta</NavLink>
+              </li>
+            </ul>
+          </NavBar>
 
-        <ProfileItem>
-          <img src={ProfilePic} alt="Perfil" />
+          <ProfileItem>
+            <img src={ProfilePic} alt="Perfil" />
 
-          <ProfileOptions>
-            <p>a</p>
-          </ProfileOptions>
-        </ProfileItem>
+            <ProfileOptions>
+              <p>a</p>
+            </ProfileOptions>
+          </ProfileItem>
+        </HeaderItems>
       </HeaderContainer>
     </>
   )
