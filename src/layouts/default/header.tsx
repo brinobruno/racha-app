c468e2e362e950c6da6ce669eb2426998b2c3f49
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-22 23:25:12 -0300

    feat: Create default layout for header (logged-in)

--- src/layouts/default/header.tsx
+++ src/layouts/default/header.tsx
@@ -1,11 +1,24 @@
 import { NavLink } from 'react-router-dom'
 
+import LogoWhite from 'src/assets/logo-white.svg'
+import ProfilePic from 'src/assets/profile-pic.png'
+import { HeaderContainer, Logo, NavBar, ProfileItem } from './styles'
+
 export function Header() {
   return (
     <>
-      <header>
-        <nav>
+      <HeaderContainer>
+        <Logo>
+          <NavLink to="/">
+            <img src={LogoWhite} alt="Início" />
+          </NavLink>
+        </Logo>
+
+        <NavBar>
           <ul>
+            <li>
+              <NavLink to="/">Início</NavLink>
+            </li>
             <li>
               <NavLink to="/dashboard">Dashboard</NavLink>
             </li>
@@ -13,8 +26,12 @@ export function Header() {
               <NavLink to="/account">Account</NavLink>
             </li>
           </ul>
-        </nav>
-      </header>
+        </NavBar>
+
+        <ProfileItem>
+          <img src={ProfilePic} alt="Perfil" />
+        </ProfileItem>
+      </HeaderContainer>
     </>
   )
 }
