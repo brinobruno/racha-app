078f796fbe6dd8d43be72865fdf987360a836465
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-25 15:53:56 -0300

    feat: Change player image based on theme

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,15 @@
+import { useThemeContext } from 'src/contexts/ThemeContext'
 import { Container } from './styles'
 
 import PlayerLight from 'src/assets/player-light.svg'
+import PlayerDark from 'src/assets/player-dark.svg'
 
 export function Dashboard() {
+  const { theme } = useThemeContext()
+
   return (
     <Container>
-      <img src={PlayerLight} alt="" />
+      <img src={theme === 'light' ? PlayerDark : PlayerLight} alt="" />
 
       <h1>Você ainda não possui times ou cartas registradas.</h1>
 
