04baf187ce7e2f35578cda631e7cc82acb024431
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-03-30 16:22:03 -0300

    refactor: Destructure data on SignInForm

--- src/components/SignInForm/index.tsx
+++ src/components/SignInForm/index.tsx
@@ -53,17 +53,15 @@ export function SignInForm() {
       email,
       password,
     })
-    const data = response.data
+    const { user, sessionId } = response.data
 
-    await addCookie(USER_SESSION_STORAGE_KEY, data.sessionId)
+    await addCookie(USER_SESSION_STORAGE_KEY, sessionId)
 
     addUser({
-      id: data.user.id,
-      username: data.user.username,
-      email: data.user.email,
+      id: user.id,
+      username: user.username,
+      email: user.email,
     })
-
-    return data
   })
 
   const signInUser = (inputs: ISignInRequest) => {
