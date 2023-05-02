afa819333a01e8c5943afcd81a21bdc4f10c612c
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-05-02 15:55:31 -0300

    feat: Create draft for TeamsContext

---
+++ src/contexts/TeamsContext.tsx
@@ -0,0 +1,59 @@
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useState,
+} from 'react'
+
+export interface IPlayer {
+  id: string
+  team_id: string
+  name: string
+  known_as: string
+  position: string
+  nationality: string
+  overall: number
+  pace: number
+  shooting: number
+  passing: number
+  dribbling: number
+  defending: number
+  physical: number
+}
+
+export interface ITeam {
+  id: string
+  title: string
+  owner: string
+
+  players: IPlayer[]
+}
+
+interface ITeams {
+  team: ITeam[]
+}
+
+interface ITeamsContext {
+  teams: ITeams | null
+  setTeams: Dispatch<SetStateAction<ITeams | null>>
+}
+
+interface ITeamsProviderProps {
+  children: ReactNode
+}
+
+export const TeamsContext = createContext<ITeamsContext>({
+  teams: null,
+  setTeams: () => {},
+})
+
+export const TeamsContextProvider = ({ children }: ITeamsProviderProps) => {
+  const [teams, setTeams] = useState<ITeams | null>(null)
+
+  return (
+    <TeamsContext.Provider value={{ teams, setTeams }}>
+      {children}
+    </TeamsContext.Provider>
+  )
+}
