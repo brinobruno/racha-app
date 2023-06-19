8d216c21ee4ce5a1a071c65acbab781bbf9febd4
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-06-19 18:29:40 -0300

    feat: Parcially develop useTeams

--- src/services/hooks/useTeams.ts
+++ src/services/hooks/useTeams.ts
@@ -2,11 +2,8 @@ import { useQuery } from 'react-query'
 import { Cookies } from 'typescript-cookie'
 
 import { api } from 'src/services/api'
-import {
-  // USER_ID_STORAGE_KEY,
-  USER_SESSION_STORAGE_KEY,
-  headers,
-} from 'src/constants'
+import { USER_SESSION_STORAGE_KEY, headers } from 'src/constants'
+import { ITeam } from 'src/contexts/TeamsContext'
 
 interface ITeamData {
   id: string
@@ -30,21 +27,36 @@ async function getTeamsData(): Promise<TeamDataResponse> {
     Cookies.get(USER_SESSION_STORAGE_KEY),
   ])
 
-  const response = await api.get(`/users/teams/all/${userId}`, {
+  const teamsResponse = await api.get(`/users/teams/all/${userId}`, {
     headers: {
       ...headers,
-      // eslint-disable-next-line prettier/prettier
-      'Cookies': `${sessionIdValue}`,
+      Cookies: `${sessionIdValue}`,
     },
   })
 
-  const data = response.data
+  const teams = teamsResponse.data.teams || []
 
-  return data
+  const teamsWithPlayers = await Promise.all(
+    teams.map(async (team: ITeam) => {
+      const playersResponse = await api.get(`/users/teams/players/${team.id}`, {
+        headers: {
+          ...headers,
+          Cookies: `${sessionIdValue}`,
+        },
+      })
+      const players = playersResponse.data.players || []
+      return {
+        ...team,
+        players,
+      }
+    }),
+  )
+
+  return { teams: teamsWithPlayers }
 }
 
 export function useTeams() {
-  return useQuery('teams', getTeamsData, {
+  return useQuery<TeamDataResponse>('teams', getTeamsData, {
     staleTime: 1000 * 60 * 5, // Will be fresh for 5 minutes
   })
 }
