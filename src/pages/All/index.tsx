3f3e081cb828eeb9d7c7583b19343ebfcb9f56a5
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-06-19 18:15:07 -0300

    feat: Add All pages

---
+++ src/pages/All/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from './styles'
+import { usePlayersFromTeam } from 'src/services/hooks/usePlayersFromTeam'
+
+export function All() {
+  const { data, isLoading, error } = usePlayersFromTeam(
+    'f40011b3-db40-4922-a26c-70a8b12da9a2',
+  )
+
+  console.log(data)
+
+  const players = data?.players
+
+  return (
+    <Container>
+      <h1>Meus times</h1>
+
+      <div>
+        {isLoading && <span>Carregando...</span>}
+        {typeof error === 'string' && <span>Erro: {error}</span>}
+
+        {players && (
+          <ul>
+            {players.map((player) => (
+              <li key={player.id}>
+                <div>ID: {player.id}</div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <button>Criar time</button>
+    </Container>
+  )
+}
