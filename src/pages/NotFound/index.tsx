5ab63209e8b823b66c1b927fc84b1a9d584c6b25
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-06-26 20:07:30 -0300

    chore: Add components/images/styles to 404 page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,8 +1,13 @@
-import { Container } from './styles'
+import { Container, IllustrationContainer, Illustration } from './styles'
+import NotFoundImg from 'src/assets/not-found.svg'
 
 export default function NotFound() {
   return (
     <Container>
+      <IllustrationContainer>
+        <Illustration src={NotFoundImg} alt="" />
+      </IllustrationContainer>
+
       <h1>Oops! You seem to be lost.</h1>
     </Container>
   )
