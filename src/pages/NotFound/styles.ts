f9c57589d22fc4a64ef9b1b97198e2d22567621f
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-06-26 20:07:14 -0300

    chore: Export styled components for notfound page

--- src/pages/NotFound/styles.ts
+++ src/pages/NotFound/styles.ts
@@ -6,3 +6,15 @@ export const Container = styled.section`
   align-items: center;
   justify-content: center;
 `
+
+export const IllustrationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  max-width: 70%;
+`
+
+export const Illustration = styled.img`
+  width: 100%;
+`
