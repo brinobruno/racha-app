f72521932b92852389d302804971e1e5389972e7
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-06-29 16:10:40 -0300

    feat: Add authHeader method

---
+++ src/services/auth/authHeader.ts
@@ -0,0 +1,32 @@
+import { USER_JWT_AUTH_TOKEN_KEY } from 'src/constants'
+
+const getTokenFromCookies = () => {
+  const jwtTokenCookieName = USER_JWT_AUTH_TOKEN_KEY
+
+  // Retrieve all cookies
+  const cookies = document.cookie.split(';')
+
+  // Find correct cookie
+  const jwtTokenCookie = cookies.find((cookie) => {
+    const [name] = cookie.trim().split('=')
+    return name === jwtTokenCookieName
+  })
+
+  // If the token cookie is found, extract and return it
+  if (jwtTokenCookie) {
+    const [, token] = jwtTokenCookie.trim().split('=')
+    return token
+  }
+
+  return console.log('Cookie not found')
+}
+
+export const authHeader = () => {
+  const token = getTokenFromCookies()
+
+  return {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  }
+}
