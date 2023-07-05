d61ca2534ccd8d8cff1ead2a80491cf45ed22eed
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-07-05 19:58:14 -0300

    chore: Some structure and assets usage

--- src/layouts/default/footer/index.tsx
+++ src/layouts/default/footer/index.tsx
@@ -1,9 +1,32 @@
+import { Link } from 'react-router-dom'
+
+import AppStoreBadge from 'src/assets/social/app-store-badge.svg'
+import GooglePlayBadge from 'src/assets/social/google-play-badge.png'
 import { Container, WidthWrapper } from './styles'
 
 export function Footer() {
   return (
     <Container>
-      <WidthWrapper>Footer</WidthWrapper>
+      <WidthWrapper>
+        <div>
+          <Link to="/">
+            <img src="" alt="" loading="lazy" />
+            Some description
+          </Link>
+        </div>
+
+        <div>
+          <span>Get the app.</span>
+
+          <Link to="/">
+            <img src={AppStoreBadge} alt="" loading="lazy" />
+          </Link>
+
+          <Link to="/">
+            <img src={GooglePlayBadge} alt="" loading="lazy" />
+          </Link>
+        </div>
+      </WidthWrapper>
     </Container>
   )
 }
