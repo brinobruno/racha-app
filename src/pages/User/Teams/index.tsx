bd91af8e33e9bb58d9ffc071c403058699d3f5a1
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-07-06 19:14:19 -0300

    docs: Remove commented-out code

--- src/pages/User/Teams/index.tsx
+++ src/pages/User/Teams/index.tsx
@@ -42,18 +42,6 @@ export function Teams() {
                   </TeamDataDivider>
 
                   <CaretRight size={28} weight="bold" color="#E9EDF5" />
-                  {/* {team.players.map((player) => (
-                  <PlayerContainer key={player.id}>
-                    <strong>Player: </strong>
-
-                    <span>
-                      {player.name}
-                      <br />
-                      {player.overall}
-                      <br />
-                    </span>
-                  </PlayerContainer>
-                ))} */}
                 </Link>
               </TeamContainer>
             ))}
