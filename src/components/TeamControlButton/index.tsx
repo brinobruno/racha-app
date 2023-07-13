87411820845ed7ffaf84179c91f5aace1dea5387
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-07-12 21:02:58 -0300

    feat: Create teamcontrolbutton

---
+++ src/components/TeamControlButton/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+
+export function TeamControlButton() {
+  return (
+    <Container>
+      <img src="" alt="" />
+      <span></span>
+    </Container>
+  )
+}
