8b0d0950a3de05b6f5cfde852d32efecb0708f9d
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-07-27 20:55:14 -0300

    chore: Utilize useUserHasTeams

--- src/pages/User/Dashboard/index.tsx
+++ src/pages/User/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { useThemeContext } from 'src/contexts/ThemeContext'
+import { useUserHasTeams } from 'src/services/hooks/useUserHasTeams'
 import { Container } from './styles'
 
 import PlayerLight from 'src/assets/player-light.svg'
@@ -6,12 +7,17 @@ import PlayerDark from 'src/assets/player-dark.svg'
 
 export function Dashboard() {
   const { theme } = useThemeContext()
+  const userHasTeams = useUserHasTeams()
 
   return (
     <Container>
       <img src={theme === 'light' ? PlayerDark : PlayerLight} alt="" />
 
-      <h1>Você ainda não possui times ou cartas registradas.</h1>
+      {userHasTeams ? (
+        <h1>User has teams</h1>
+      ) : (
+        <h1>Você ainda não possui times ou cartas registradas.</h1>
+      )}
 
       <button>Criar time</button>
     </Container>
