d76e53dba707555ca3447a55ca6c6fd61368c274
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-08-04 21:57:14 -0300

    chore: Add styles to HeaderContainer

--- src/components/GlobalModal/styles.ts
+++ src/components/GlobalModal/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
   svg {
     cursor: pointer;
   }
