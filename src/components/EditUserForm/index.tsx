ac42e8ac5c000dc25d2bfd5d80a3d2fa027f6042
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-08-14 20:26:52 -0300

    feat: Add EditUserForm base

---
+++ src/components/EditUserForm/index.tsx
@@ -0,0 +1,109 @@
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from 'react-query'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { AxiosError } from 'axios'
+import { toast } from 'react-toastify'
+
+import { api } from 'src/services/api'
+import { FieldContainer, Form, Validate, WarningsContainer } from './styles'
+import { Cookies } from 'typescript-cookie'
+import { USER_ID_STORAGE_KEY } from 'src/constants'
+
+interface IEditUserRequest {
+  username: string
+}
+
+const editUserFormValidationSchema = zod.object({
+  username: zod
+    .string()
+    .min(2, 'Insira seu novo username (mínimo 2 caracteres)'),
+})
+
+export type EditUserFormData = zod.infer<typeof editUserFormValidationSchema>
+
+export function EditUserForm() {
+  const userId = Cookies.get(USER_ID_STORAGE_KEY)
+
+  const newEditUserForm = useForm<EditUserFormData>({
+    resolver: zodResolver(editUserFormValidationSchema),
+    defaultValues: {
+      username: '',
+    },
+  })
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = newEditUserForm
+  const handleEditUserSubmit: SubmitHandler<IEditUserRequest> = (
+    editUserInputs,
+  ) => {
+    handleEditUser(editUserInputs)
+  }
+
+  const editUser = useMutation(async ({ username }: IEditUserRequest) => {
+    const { data } = await api.put(`/users/${userId}`, {
+      username,
+    })
+
+    console.log(data)
+  })
+
+  const handleEditUser = (inputs: IEditUserRequest) => {
+    editUser.mutate(inputs, {
+      onSuccess: async () => {
+        reset()
+
+        toast('Usuário atualizado com sucesso')
+      },
+      onError: () => {
+        toast('Revise os detalhes', {
+          className: 'warning-toast',
+        })
+      },
+    })
+  }
+
+  let editUserError
+  if (editUser.error instanceof AxiosError)
+    editUserError = editUser.error.response?.data.error
+
+  return (
+    <Form onSubmit={handleSubmit(handleEditUserSubmit)}>
+      <FieldContainer>
+        <label htmlFor="username">Username</label>
+        <input
+          required
+          id="username"
+          {...register('username')}
+          name="username"
+          autoFocus
+        />
+      </FieldContainer>
+
+      {errors?.username && <Validate>{errors.username.message}</Validate>}
+
+      <button
+        data-testid="edit-user-submit"
+        disabled={editUser.isLoading}
+        type="submit"
+      >
+        Atualizar
+      </button>
+
+      <WarningsContainer>
+        {editUser.isLoading && <span>Editando dados...</span>}
+        {editUser.isSuccess && <span>Dados atualizados com sucesso</span>}
+        {editUser.isError && (
+          <span>
+            Erro ao atualizar seus dados: <br />
+            {editUserError}
+          </span>
+        )}
+      </WarningsContainer>
+    </Form>
+  )
+}
---
+++ src/components/EditUserForm/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  width: 100%;
+
+  button {
+    margin-top: 1rem;
+    width: 100%;
+  }
+`
+
+export const FieldContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  width: 100%;
+
+  label {
+    font-weight: 600;
+  }
+
+  input {
+    background: ${(props) => props.theme['neutral-700']};
+    outline: ${(props) => props.theme['neutral-700']};
+    color: ${(props) => props.theme['neutral-100']};
+    transition: 0.15s outline ease-in-out, 0.15s color ease-in-out;
+
+    &:focus {
+      outline: 1px solid ${(props) => props.theme['secondary-500']};
+      color: ${(props) => props.theme['secondary-300']};
+    }
+  }
+`
+
+export const WarningsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    text-align: center;
+  }
+`
+
+export const Validate = styled.span`
+  font-size: 0.875rem;
+`
