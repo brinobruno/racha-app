0ad63170fc2d651054ff1e9a1b8e96b10d080049
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-08-23 20:18:50 -0300

    feat: Add base swiper

--- src/components/TeamViews/SwiperView/index.tsx
+++ src/components/TeamViews/SwiperView/index.tsx
@@ -1,6 +1,12 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { A11y, Navigation, Pagination } from 'swiper/modules'
+
 import { IPlayerData } from 'src/services/hooks/usePlayersFromTeam'
 import { Card } from 'src/components/Card'
 import { Container } from './styles'
+
+import 'swiper/css'
+import 'swiper/css/pagination'
 
 interface IListViewProps {
   players: IPlayerData[]
@@ -9,22 +15,29 @@ interface IListViewProps {
 export function SwiperView({ players }: IListViewProps) {
   return (
     <Container>
-      {players.map((player) => (
-        <div key={player.id}>
-          <Card
-            overall={player.overall}
-            position={player.position}
-            nationality={player.nationality}
-            name={player.known_as}
-            pace={player.pace}
-            dribbling={player.dribbling}
-            shooting={player.shooting}
-            defending={player.defending}
-            passing={player.passing}
-            physical={player.physical}
-          />
-        </div>
-      ))}
+      <Swiper
+        modules={[A11y, Navigation, Pagination]}
+        pagination={{ clickable: true }}
+        slidesPerView={'auto'}
+        rewind
+      >
+        {players.map((player) => (
+          <SwiperSlide key={player.id}>
+            <Card
+              overall={player.overall}
+              position={player.position}
+              nationality={player.nationality}
+              name={player.known_as}
+              pace={player.pace}
+              dribbling={player.dribbling}
+              shooting={player.shooting}
+              defending={player.defending}
+              passing={player.passing}
+              physical={player.physical}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </Container>
   )
 }
