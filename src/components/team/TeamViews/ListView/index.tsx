f9f3d774175669aa478ec10e7d1413ae77ed182e
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-08-31 15:28:39 -0300

    feat: Add styled table

--- src/components/team/TeamViews/ListView/index.tsx
+++ src/components/team/TeamViews/ListView/index.tsx
@@ -1,5 +1,18 @@
 import { IPlayerData } from 'src/services/hooks/usePlayersFromTeam'
-import { Container, ListHeaders, ListData } from './styles'
+import {
+  PlayerTable,
+  ListHeaders,
+  ListData,
+  PlayerNationality,
+  PlayerPicture,
+  DataBigger,
+  Container,
+  PlayerSummary,
+  HeadingSmaller,
+  HeadingBigger,
+  DataSmaller,
+} from './styles'
+import { getCode } from 'country-list'
 
 interface IListViewProps {
   players: IPlayerData[]
@@ -8,21 +21,48 @@ interface IListViewProps {
 export function ListView({ players }: IListViewProps) {
   return (
     <Container>
-      {/* Implement filters by clicking on a table, e.g: POS, OVR */}
-      {/* Add fixed height, and scroll inside of table */}
-      <ListHeaders>
-        <th>Pos</th>
-        <th>Name</th>
-        <th>OVR</th>
-      </ListHeaders>
+      <PlayerTable>
+        {/* Implement filters by clicking on a table, e.g: POS, OVR */}
+        {/* Add fixed height, and scroll inside of table */}
+        <thead>
+          <ListHeaders>
+            <HeadingSmaller>Pos</HeadingSmaller>
+            <HeadingSmaller>País</HeadingSmaller>
+            <HeadingBigger>Nome</HeadingBigger>
+            <HeadingSmaller>OVR</HeadingSmaller>
+          </ListHeaders>
+        </thead>
 
-      {players.map((player) => (
-        <ListData key={player.id}>
-          <td>{player.position}</td>
-          <td>{player.name}</td>
-          <td>{player.overall}</td>
-        </ListData>
-      ))}
+        <tbody>
+          {players.map((player) => (
+            <ListData key={player.id}>
+              <DataSmaller>{player.position}</DataSmaller>
+              <DataSmaller>
+                <PlayerNationality
+                  src={`https://flagsapi.com/${getCode(
+                    player.nationality,
+                  )}/flat/64.png`}
+                  alt=""
+                />
+              </DataSmaller>
+              <DataBigger>
+                <PlayerPicture
+                  src="https://www.fifarosters.com/assets/players/fifa23/faces/158023.png"
+                  alt={player.name}
+                />
+                {player.name}
+              </DataBigger>
+              <DataSmaller>{player.overall}</DataSmaller>
+            </ListData>
+          ))}
+        </tbody>
+      </PlayerTable>
+
+      <PlayerSummary>
+        <strong>Player summary</strong>
+        <h2>Lionel Messi</h2>
+        <span>92 OVR</span>
+      </PlayerSummary>
     </Container>
   )
 }
--- src/components/team/TeamViews/ListView/styles.ts
+++ src/components/team/TeamViews/ListView/styles.ts
@@ -1,22 +1,77 @@
 import styled from 'styled-components'
 
-export const Container = styled.table`
-  width: 100%;
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+
+  height: 100%;
+`
+
+export const PlayerTable = styled.table`
+  width: 65%;
+  border-spacing: unset;
 `
 
 export const ListHeaders = styled.tr`
   background: ${(props) => props.theme['secondary-600']};
 
   th {
+    font-size: 1.125rem;
+    font-weight: 500;
     text-align: left;
-    padding: 0.5rem;
+
+    padding: 0.75rem;
   }
 `
 
 export const ListData = styled.tr`
   background: ${(props) => props.theme['secondary-accent']};
+  transition: 0.1s ease-in-out;
+
+  &:nth-child(even) {
+    background: ${(props) => props.theme['secondary-accent-hover']};
+  }
+
+  &:hover {
+    background: ${(props) => props.theme['secondary-550']};
+  }
 
   td {
-    padding: 0.5rem;
+    padding: 0.25rem 0.75rem;
+    max-height: 3.125rem;
   }
+`
+
+export const HeadingBigger = styled.th`
+  width: 64%;
+`
+
+export const HeadingSmaller = styled.th`
+  width: 12%;
+`
+
+export const DataBigger = styled.td`
+  display: flex;
+  align-items: center;
+`
+
+export const DataSmaller = styled.td`
+  text-align: center;
+`
+
+export const PlayerNationality = styled.img`
+  width: 32px;
+`
+
+export const PlayerPicture = styled.img`
+  width: 42px;
+  height: auto;
+`
+
+export const PlayerSummary = styled.div`
+  width: 35%;
+  height: auto;
+
+  background: ${(props) => props.theme['secondary-600']};
 `
