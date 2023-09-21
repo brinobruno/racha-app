2c162db43c7ab751a507dd049c341d10695596ec
Bruno Corrêa <brunosantos6ft@gmail.com>
2023-09-20 22:02:48 -0300

    feat: Add react-beautiful-dnd to project

--- src/components/team/TeamViews/PitchView/index.tsx
+++ src/components/team/TeamViews/PitchView/index.tsx
@@ -1,30 +1,71 @@
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
 import { IPlayerData } from 'src/services/hooks/usePlayersFromTeam'
 import { Card } from 'src/components/team/Card'
 import { Container } from './styles'
+import { useState } from 'react'
 
 interface IListViewProps {
   players: IPlayerData[]
 }
 
-export function PitchView({ players }: IListViewProps) {
+export function PitchView({ players: PlayersData }: IListViewProps) {
+  const [players, updatePlayers] = useState(PlayersData)
+
+  function handleOnDragEnd(result: any) {
+    if (!result.destination) return
+
+    const items = Array.from(players)
+    const [reorderedItem] = items.splice(result.source.index, 1)
+    items.splice(result.destination.index, 0, reorderedItem)
+
+    updatePlayers(items)
+  }
+
   return (
     <Container>
-      {players.map((player) => (
-        <div key={player.id}>
-          <Card
-            overall={player.overall}
-            position={player.position}
-            nationality={player.nationality}
-            name={player.known_as}
-            pace={player.pace}
-            dribbling={player.dribbling}
-            shooting={player.shooting}
-            defending={player.defending}
-            passing={player.passing}
-            physical={player.physical}
-          />
-        </div>
-      ))}
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="players">
+          {(provided) => (
+            <div
+              className="players"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {players.map((player, index) => {
+                return (
+                  <Draggable
+                    key={player.id}
+                    draggableId={player.id}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <Card
+                          overall={player.overall}
+                          position={player.position}
+                          nationality={player.nationality}
+                          name={player.known_as}
+                          pace={player.pace}
+                          dribbling={player.dribbling}
+                          shooting={player.shooting}
+                          defending={player.defending}
+                          passing={player.passing}
+                          physical={player.physical}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                )
+              })}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </Container>
   )
 }
